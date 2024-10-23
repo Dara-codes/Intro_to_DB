f032de95e3f3e729dda3c79369a3af6710bbc133
DaraJoe <deejayblinks@gmail.com>
2024-10-23 04:05:56 +0100

    introduction to batabases, SQL

--- task_4.sql
+++ task_4.sql
@@ -4,4 +4,4 @@ USE alx_book_store;
 -- Query the INFORMATION_SCHEMA to get the full description of the books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
